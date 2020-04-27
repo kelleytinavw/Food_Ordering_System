e636009e99b619260563ccc26bae7a740820a68f
AdrianTJenkins <adrian.jenkins99@gmail.com>
2020-04-26 23:32:28 -0400

    reworked template

--- server.js
+++ server.js
@@ -173,7 +173,7 @@ MongoClient.connect('mongodb+srv://adrian:csc570@cluster0-onki1.mongodb.net/test
         .catch(error => console.error(error))
     })
 
-    app.get('/home.html', (req, res) => {
+    app.get('/', (req, res) => {
         db.collection('menus').find().toArray()
         .then(results => {
             res.render('home.ejs', {menus: results})
